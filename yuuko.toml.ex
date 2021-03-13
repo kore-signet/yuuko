fdabf1c78baed0bc8b8867d6ae52711f5646f6e2
dannybd <dannybd@mit.edu>
2021-03-13 18:13:23 +0000

    Update yuuko.toml.ex with default crisp API

--- yuuko.toml.ex
+++ yuuko.toml.ex
@@ -1,13 +1,13 @@
 [miku]
-token = '' # discord token
-prefix = 'salmon!' # discord command prefix
-cache_path = 'db/cache.db' # player cache database
+token = "" # discord token
+prefix = "salmon!" # discord command prefix
+cache_path = "db/cache.db" # player cache database
 extinction_messages = [
-  'congratz, your fave is an environmental criminal',
-  'salmon is now Unstable!',
-  "which means they're about as good Jessica Telephone at this"
+  "congratz, your fave is an environmental criminal",
+  "salmon is now Unstable!",
+  "which means they're about as good Jessica Telephone at this",
 ]
 
 [salmon]
 total_sims = 20 # total simulation cases to run on command
-api_url = 'https://fish' # WEBCrisp API url (include endpoint!)
+api_url = "https://crisp.sibr.dev/api/sim" # WEBCrisp API url (include endpoint!)
