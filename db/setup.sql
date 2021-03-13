80d66ee4c3826e7db70e5a297c53bb80a4de00b1
alisw <waylandalis@gmail.com>
2021-03-13 17:16:29 -0300

    add results archival

--- db/setup.sql
+++ db/setup.sql
@@ -2,3 +2,9 @@ CREATE TABLE IF NOT EXISTS player_cache (
   name TEXT,
   stlats TEXT
 );
+
+CREATE TABLE IF NOT EXISTS result_archive (
+  player TEXT,
+  result BLOB,
+  timestamp INTEGER -- this is UTC
+);
